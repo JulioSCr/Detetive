0d02482eacac2e459b3264dd592487e2cdcde6b3
Lucas Batista <lucas.bastos@outlook.com>
2020-05-10 21:47:10 -0300

    Adição projeto base da API utilizando ASP.NET Core v3.1.

---
+++ API/Detetive/Detetive.API/Configuration/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Detetive.API.Configuration
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection ResolveDependecies(this IServiceCollection services)
+        {
+            //services.AddScoped<INotificador, Notificador>();
+
+            return services;
+        }
+    }
+}
