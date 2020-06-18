50f79aec6bc2c10f26c044c6336d386cfb33092e
Lucas Batista <lucas.bastos@outlook.com>
2020-06-17 22:56:07 -0300

    Merge com a develop.

--- Detetive/Detetive/Controllers/HomeController.cs
+++ Detetive/Detetive/Controllers/HomeController.cs
@@ -9,30 +9,12 @@ namespace Detetive.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ISuspeitoRepository _suspeitoRepository;
-
-        public HomeController(ISuspeitoRepository suspeitoRepository)
+        public HomeController()
         {
-            _suspeitoRepository = suspeitoRepository;
         }
 
         public ActionResult Index()
         {
-            //var lista = _suspeitoRepository.Listar();
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
--- Detetive/Detetive/Controllers/SalaController.cs
+++ Detetive/Detetive/Controllers/SalaController.cs
@@ -20,54 +20,30 @@ namespace Detetive.Controllers
             _jogadorSalaBusiness = jogadorSalaBusiness;
         }
 
-        public void Manter(string dsJogador)
-        {
-            var jogador = _jogadorBusiness.Adicionar(dsJogador);
-
-            var sala = _salaBusiness.Adicionar(jogador.Id);
-
-            var operacao = _jogadorSalaBusiness.Adicionar(sala, jogador.Id);
-        }
-
-        public void Ingressar(int idSala, int idJogador)
-        {
-            var sala = _salaBusiness.Obter(idSala);
-
-            if (sala == default)
-                return;
-
-            var operacao = _jogadorSalaBusiness.Adicionar(sala, idJogador);
-        }
-    }
-}
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace Detetive.Controllers
-{
-    public class SalaController : Controller
-    {
-        // GET: ManterSala()
         public ActionResult Index()
         {
             return View();
         }
+
         public ActionResult Manter()
         {
+            //var jogador = _jogadorBusiness.Adicionar(dsJogador);
+
+            //var sala = _salaBusiness.Adicionar(jogador.Id);
+
+            //var operacao = _jogadorSalaBusiness.Adicionar(sala, jogador.Id);
+
             return View();
         }
-        public string CriarSala()
-        {
-            //object lRetorno = new
-            //{
-            //    IDSala = 1234,
-            //    Status = "Ok"
-            //};
 
-            return "1234";
+        public void Ingressar(int idSala, int idJogador)
+        {
+            //var sala = _salaBusiness.Obter(idSala);
+
+            //if (sala == default)
+            //    return;
+
+            //var operacao = _jogadorSalaBusiness.Adicionar(sala, idJogador);
         }
     }
 }
