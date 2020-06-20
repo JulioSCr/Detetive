761a4cb8c969cceea482c36749a9e7b79a94b448
Rafael Alcantara Carreiro Gallo <rafael_g12@hotmail.com>
2020-06-20 12:12:15 -0300

    inclusão de iframe propaganda

---
+++ Detetive/Detetive/Scripts/Views/Propaganda/Ad.js
@@ -0,0 +1,23 @@
+﻿
+var myVideo = document.getElementById("ad"); 
+
+
+window.onload = function () {
+    EsconderAd()
+    setTimeout(ExibirAd, 5000)
+    setTimeout(EsconderAd, 15000)
+};
+
+
+function ExibirAd() {
+    document.getElementById('propaganda').style.visibility = 'visible';
+    myVideo.play();
+    myVideo.autoplay()
+}
+
+function EsconderAd() {
+    document.getElementById('propaganda').style.visibility = 'hidden';
+    myVideo.pause()
+}
+
+
