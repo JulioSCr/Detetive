ca3c5a1db1bf15310559c47d6f92f5372d811ee9
Julio Silva Cruz <julioscruzny@gmail.com>
2020-07-12 16:23:57 -0300

    Processo: Listar suspeitos Descrição: Ajuste para apenas o criador da partida poder inicia-la

---
+++ Detetive/Detetive/Mappers/MapperSalaToSalaViewModel.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Detetive.Business.Entities;
+using Detetive.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Detetive.Mappers
+{
+    public class MapperSalaToSalaViewModel : Profile
+    {
+        public MapperSalaToSalaViewModel()
+        {
+            CreateMap<Sala, SalaViewModel>()
+                .ForMember(viewModel => viewModel.Id, _ => _.MapFrom(model => model.Id))
+                .ForMember(viewModel => viewModel.DataCriacao, _ => _.MapFrom(model => model.DataCriacao))
+                .ForMember(viewModel => viewModel.Ativo, _ => _.MapFrom(model => model.Ativo))
+                .ForMember(viewModel => viewModel.IdJogadorSala, _ => _.MapFrom(model => model.IdJogadorSala));
+        }
+
+        public override string ProfileName
+        {
+            get { return "MapperSalaToSalaViewModel"; }
+        }
+    }
+}
